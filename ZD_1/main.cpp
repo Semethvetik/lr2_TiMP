8eb6310be0740b7cd6a1758b2f797f2606aea659
Semethvetik <146645448+Semethvetik@users.noreply.github.com>
2024-01-16 13:20:39 +0300

    Update main.cpp

--- ZD_1/main.cpp
+++ ZD_1/main.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
+#include <cctype>
 #include "modAlphaCipher.h"
+#include <locale>
+#include <codecvt>
+#include <typeinfo>
 using namespace std;
 
-void check(const wstring& Text, const wstring& key)
+void check(const wstring& Text, const wstring& key, const bool destructCipherText = false)
 {
-    wstring cipherText;
-    wstring decryptedText;
-
     try {
+        wstring cipherText;
+        wstring decryptedText;
         modAlphaCipher cipher(key);
         cipherText = cipher.encrypt(Text);
+        if (destructCipherText)
+            cipherText.front() = towlower(cipherText.front());
         decryptedText = cipher.decrypt(cipherText);
-        wcout << L"key=" << key <<endl;
-        wcout << Text << endl;
-        wcout << cipherText << endl;
-        wcout << decryptedText << endl;
-        if (Text == decryptedText)
-            wcout << L"Ok\n";
-        else
-            wcout << L"Err\n";
+        wcout << L"\nkey = " << key << endl;
+        wcout << L"Text = " << Text << endl;
+        wcout << L"Encrypted text: " << cipherText << endl;
+        wcout << L"Decrypted text: " << decryptedText << "\n" << endl;
     } catch (const cipher_error & e) {
-        cerr << "Error: " << e.what() << endl;
+        wcerr << "Error: " << e.what() << endl;
     }
 }
 
@@ -28,9 +29,8 @@ int main()
 {
     locale loc("ru_RU.UTF-8");
     locale::global(loc);
-
-    check(L"ПЕНЗАКЛАССНЫЙГОРОД", L"КЛЮЧ");
+    check(L"ПЕНЗА", L"КЛЮЧИ");
     check(L"ПЕНЗАКЛАССНЫЙГОРОД", L"");
+    check(L"ТЁМАТЁМА",L"ЛЮБОВ");
     check(L"ПЕНЗА КЛАССНЫЙ ГОРОД", L"КЛЮЧ");
-    return 0;
 }
